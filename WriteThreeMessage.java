29d52e1301b5ca0e9495189b9d0ce9fd84898e33
Yi <bigyi1989@gmail.com>
2015-03-14 13:18:35 -0400

    updates

---
+++ WriteThreeMessage.java
@@ -0,0 +1,9 @@
+/*1.1 (Display three messages) Write a program that displays Welcome to Java,
+Welcome to Computer Science, and Programming is fun.*/
+public class WriteThreeMessage{
+	public static void main(String[] args){
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+	}
+}
---
+++ second_try.java
@@ -0,0 +1,21 @@
+public class WelcomeWithThreeMessages{
+public static void main(String[] args){
+system.out.println("programming is fun!");
+system.out.println("fundamentals first");
+system.out.println("problem driven");
+} 
+}
+
+public class ComputeExpression{
+	public static void main (sting[]args){
+		system.out.println(10.5+2*3)/(45-3.5)
+	}
+}
+
+public class Test{
+	public static void main(string[] args){
+		system.out.println("3.5*4/2-2.5 is ");
+		system.out.println(3.5*4/2-2.5);
+	}
+}
+
